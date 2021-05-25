c36af28019120a3750c2e9e2668b6d4e42ee9c7e
Ilya <eli.kaplan70@gmail.com>
2021-05-25 17:58:03 +0300

    updated entrypoint to use port argument

--- entry-point.sh
+++ entry-point.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-export port=8080
+if [ $# -q 1 ]
+then
+    echo "###############################"
+    echo "Recieved argument, PORT=$1"
+    echo "###############################"
+    export PORT=$1
+else
+    echo "###############################"
+    echo "Did not revieve an argument, default PORT=8080"
+    echo "###############################"
+fi
 
+echo "###############################"
+echo "STARTIG SERVER"
+echo "###############################"
+ 
 npm start
